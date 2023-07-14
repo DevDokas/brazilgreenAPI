2e5522496d0bfedd0c08641ed0265202ba029844
Igor Dokai <igordokai@outlook.com>
2023-07-14 16:53:49 -0300

    first commit

---
+++ src/main/java/com/igordokai/brazilgreen/BrazilgreenApplication.java
@@ -0,0 +1,13 @@
+package com.igordokai.brazilgreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrazilgreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrazilgreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igordokai/brazilgreen/BrazilgreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igordokai.brazilgreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrazilgreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
