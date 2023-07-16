0475aab98582c8db9f807403119998bd3374c6bd
Dokas <iguinhogms@gmail.com>
2023-07-16 15:49:28 -0300

    crud "positive effects" done

---
+++ src/main/java/com/igordokai/brazilgreen/controllers/PositiveEffectsController.java
@@ -0,0 +1,46 @@
+package com.igordokai.brazilgreen.controllers;
+
+import com.igordokai.brazilgreen.entities.PositiveEffects;
+import com.igordokai.brazilgreen.services.PositiveEffectsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/strain/positive-effects")
+public class PositiveEffectsController {
+    private final PositiveEffectsService service;
+
+    @GetMapping
+    public ResponseEntity<List<PositiveEffects>> getData() {
+        return ResponseEntity.ok(service.getData());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Optional<PositiveEffects>> getDataById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getDataById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> postData(@RequestBody PositiveEffects positiveEffects) {
+        service.postData(positiveEffects);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<?> putData(@PathVariable Long id, @RequestBody PositiveEffects positiveEffects) {
+        service.putData(id, positiveEffects);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> deleteData(@PathVariable Long id) {
+        service.deleteData(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
--- src/main/java/com/igordokai/brazilgreen/services/PositiveEffectsService.java
+++ src/main/java/com/igordokai/brazilgreen/services/PositiveEffectsService.java
@@ -1,4 +1,44 @@
 package com.igordokai.brazilgreen.services;
 
+import com.igordokai.brazilgreen.entities.PositiveEffects;
+import com.igordokai.brazilgreen.repositories.PositiveEffectsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class PositiveEffectsService {
+    private final PositiveEffectsRepository positiveEffectsRepository;
+
+    public boolean existsById(Long id) {
+        return positiveEffectsRepository.existsById(id);
+    }
+
+    public List<PositiveEffects> getData() {
+        return positiveEffectsRepository.findAll();
+    }
+
+    public Optional<PositiveEffects> getDataById(Long id) {
+        return positiveEffectsRepository.findById(id);
+    }
+
+    public void postData(PositiveEffects positiveEffects) {
+        positiveEffectsRepository.save(positiveEffects);
+    }
+
+    public void putData(Long id, PositiveEffects positiveEffects) {
+        if (existsById(id)) {
+            positiveEffects.setId(id);
+            positiveEffectsRepository.save(positiveEffects);
+        }
+    }
+
+    public void deleteData(Long id) {
+        if (existsById(id)) {
+            positiveEffectsRepository.deleteById(id);
+        }
+    }
 }
