514e7c36429f2afbea3b3e42781fe36e1a8f382d
Dokas <iguinhogms@gmail.com>
2023-07-16 23:55:15 -0300

    crud "terpenes" done

---
+++ src/main/java/com/igordokai/brazilgreen/controllers/TerpenesController.java
@@ -0,0 +1,46 @@
+package com.igordokai.brazilgreen.controllers;
+
+import com.igordokai.brazilgreen.entities.Terpenes;
+import com.igordokai.brazilgreen.services.TerpenesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/strain/terpenes")
+@RequiredArgsConstructor
+public class TerpenesController {
+    private final TerpenesService service;
+
+    @GetMapping
+    public ResponseEntity<List<Terpenes>> getData() {
+        return ResponseEntity.ok(service.getData());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Optional<Terpenes>> getDataById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getDataById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> postData(@RequestBody Terpenes terpenes) {
+        service.postData(terpenes);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<?> putData(@PathVariable Long id, @RequestBody Terpenes terpenes) {
+        service.putData(id, terpenes);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> deleteData(@PathVariable Long id) {
+        service.deleteData(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
--- src/main/java/com/igordokai/brazilgreen/services/TerpenesService.java
+++ src/main/java/com/igordokai/brazilgreen/services/TerpenesService.java
@@ -1,4 +1,44 @@
 package com.igordokai.brazilgreen.services;
 
+import com.igordokai.brazilgreen.entities.Terpenes;
+import com.igordokai.brazilgreen.repositories.TerpenesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class TerpenesService {
+    private final TerpenesRepository terpenesRepository;
+
+    public boolean existsById(Long id) {
+        return terpenesRepository.existsById(id);
+    }
+
+    public List<Terpenes> getData() {
+        return terpenesRepository.findAll();
+    }
+
+    public Optional<Terpenes> getDataById(Long id) {
+        return terpenesRepository.findById(id);
+    }
+
+    public void postData(Terpenes terpenes) {
+        terpenesRepository.save(terpenes);
+    }
+
+    public void putData(Long id, Terpenes terpenes) {
+        if (existsById(id)) {
+            terpenes.setId(id);
+            terpenesRepository.save(terpenes);
+        }
+    }
+
+    public void deleteData(Long id) {
+        if (existsById(id)) {
+            terpenesRepository.deleteById(id);
+        }
+    }
 }
